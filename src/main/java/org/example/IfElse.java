019ee067fde3ca2f4fd100a7e7188b67a87f67f3
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-27 13:17:45 +0530

    Ternary operator added in code

--- src/main/java/org/example/IfElse.java
+++ src/main/java/org/example/IfElse.java
@@ -13,13 +13,26 @@ public class IfElse {
 //            System.out.println("You can vote");
 //        }
         //------------else if----------------
-          String day = sc.next();
-        if (day.equals("Sunday") || day.equals("sunday") || day.equals("sun")) {
-            System.out.println("Holiday, Go to Home");
-        }else if(day.equals("Monday") || day.equals("Tuesday") ||day.equals("Wednesday")||day.equals("Thursday")||day.equals("Friday")||day.equals("Saturday")){
-            System.out.println("Working day");
-        }else {
-            System.out.println("Invalid input");
-        }
+//          String day = sc.next();
+//        if (day.equals("Sunday") || day.equals("sunday") || day.equals("sun")) {
+//            System.out.println("Holiday, Go to Home");
+//        }else if(day.equals("Monday") || day.equals("Tuesday") ||day.equals("Wednesday")||day.equals("Thursday")||day.equals("Friday")||day.equals("Saturday")){
+//            System.out.println("Working day");
+//        }else {
+//            System.out.println("Invalid input");
+//        }
+
+        // Ternary Operator(condition ? expression1 : expression2)-------------------
+
+        int a = 30;
+        int b = 210;
+        int c = 110;   //with 1 more variable
+        int max = 0;
+
+        //max = a > b ? a : b;  // condition ? expression1 : expression2
+        max = a > b ? a > c ? a : c : b > c ? b : c;
+        System.out.println("value of max:" +max);
+
+
     }
 }
