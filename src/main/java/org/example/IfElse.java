991d161becea55af62bf99fc3862049343a92db7
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-27 12:55:34 +0530

    Add files via upload
    if and else if done in this file

--- src/main/java/org/example/IfElse.java
+++ src/main/java/org/example/IfElse.java
@@ -3,14 +3,23 @@ import java.util.*;
 
 public class IfElse {
     public static void main(String[] args){
-
+// ------------if--------
         Scanner sc = new Scanner(System.in);
-        int age =sc.nextInt();
-        if(age<18){
-            System.out.println("You can't vote");
-        }
-        else{
-            System.out.println("You can vote");
+//        int age =sc.nextInt();
+//        if(age<18){
+//            System.out.println("You can't vote");
+//        }
+//        else{
+//            System.out.println("You can vote");
+//        }
+        //------------else if----------------
+          String day = sc.next();
+        if (day.equals("Sunday") || day.equals("sunday") || day.equals("sun")) {
+            System.out.println("Holiday, Go to Home");
+        }else if(day.equals("Monday") || day.equals("Tuesday") ||day.equals("Wednesday")||day.equals("Thursday")||day.equals("Friday")||day.equals("Saturday")){
+            System.out.println("Working day");
+        }else {
+            System.out.println("Invalid input");
         }
     }
 }
