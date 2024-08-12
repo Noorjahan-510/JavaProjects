4b4acee4095a512913125c4fb2fc03af6a0dc6c9
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-08-12 08:50:16 +0000

    Inheritence

--- src/main/oops2/Vehicle_inheritance.java
+++ src/main/oops2/Vehicle_inheritance.java
@@ -1,6 +1,14 @@
 package oops2;
-
+//public final class Vehicle_inheritance {     here it's final, car can not inherit
 public class Vehicle_inheritance {
+
+    //int gears;
+    final int gears=4;
+
+    public static void main(String[] args) {
+        Vehicle_inheritance v1= new Vehicle_inheritance(2);
+    //    v1.gears=6;  //can't update gears value bcz it's final.
+    }
     int wheelsCount;
     String model;
 
@@ -13,6 +21,10 @@ public class Vehicle_inheritance {
     }
 
     void start(){
-        System.out.println("Vehicale started");
+        System.out.println("Vehicle started");
+    }
+
+    final void acceleration(){                 // can not be override in child class bcz it's final
+        System.out.println("vehicle is accelerating");
     }
 }
